fd4a6f751447b7ad0d602d118b8ed5ec9eefe91b
Steve Nyanumba <reach@stevenyanumba.com>
2023-04-05 23:10:09 +0300

    05.04.2023.23.10

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let instruments = document.querySelector('.instrument');
-    let cartTotal = 0
-    function fetchInstruments(url) {
-        fetch(url)
-            .then(response => response.json())
-            .then(response => {
-                for (let i = 0; i < response.length; i++) {
-                    instruments.innerHTML += `
+let instruments = document.querySelector(".instrument");
+let cart = [];
+document.getElementById("cart_total").innerHTML = cart.length;
+
+document.addEventListener("DOMContentLoaded", () => {
+  function fetchInstruments(url) {
+    fetch(url)
+      .then((response) => response.json())
+      .then((response) => {
+        for (let i = 0; i < response.length; i++) {
+          instruments.innerHTML += `
                         <div class="instrument">
                             <img src="${response[i].img}" alt ="" class="product-img">
                             <div class="product-content">
@@ -14,26 +16,36 @@ document.addEventListener('DOMContentLoaded', () => {
                                 <h3 class="description">${response[i].description}</h3>
                                 <div class="price-container">
                                     <h3 class="instrument-price">ksh.${response[i].Price}.00</h3>
-                                    <a href="#!" data-instrumentId="${response[i].id}" class="add-to-cart"><i class="fa-solid fa-cart-shopping fa-beat"></i></a>
-                                    <button onClick='buyNowButton' id="buy-now" class="buy-now" role="button">Buy Now</button>
+                                    <button onclick='cart.push(${response[i].id});document.getElementById("cart_total").innerHTML = cart.length; console.log(cart)' data-instrumentId="${response[i].id}" class="add-to-cart"><i class="fa-solid fa-cart-shopping fa-beat"></i></button>
+                                    <button onclick='alert("you have successfuly bought ${cart}");'class="buy-now" role="button">Buy Now</button>
                                 </div>
                             </div>    
                         </div>`;
-                }
+        }
+      })
+      .catch((error) => console.log(error));
+  }
 
+
+  for (let i = 0; i < cart.length; i++) {
+    fetch(url)
+      .then((response) => response.json())
+      .then((response) => {
+        document.getElementById('cartItem').innerHTML=""
+        for (let j = 0; j < response.length; j++) {
+            if (cart[i] == response[j]) {
+                document.getElementById('cartItem').innerHTML+=`
                 
-            })
-            .catch(error => console.log(error));
-               
-    }
-
-    document.getElementById("buy-now", ()=> {
-        addEventListener("click", () => {
-            alert('you have successfuly bought')
-        });
+                <h5>Test Description</h5>
+                <p>KES. 12,500.00</p>
+                `
+            }
+        }
     });
-
-    fetchInstruments('https://crediblesounds.onrender.com/instruments');
-
+        
+            
     
+  }
+  fetchInstruments("https://crediblesounds.onrender.com/instruments");
+
 });
